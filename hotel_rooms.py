0e0a5b6493643c605061e51afcd1e66f29c72663
Jack Skiles <jackskiles420@gmail.com>
2020-04-07 11:41:56 -0400

    Created hotel rooms dictionary program

---
+++ hotel_rooms.py
@@ -0,0 +1,6 @@
+
+hotel_rooms = {'101': 'Jotaro', '102': 'Joseph', '103': 'kakyoin', '104': 'Avdol', '105': 'Polnereffe', '106': 'Iggy'}
+
+print(hotel_rooms)
+hotel_rooms['107'] = 'Hol Horse'
+print(hotel_rooms)
