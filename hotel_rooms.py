dd45ce00697b9f896359cdd04b32339b7591bbca
Jack Skiles <jackskiles420@gmail.com>
2020-04-07 13:20:03 -0400

    Changed hotel_rooms.py

--- hotel_rooms.py
+++ hotel_rooms.py
@@ -20,11 +20,21 @@ while True:
         hotel_rooms[new_room] = new_occupant
         print(hotel_rooms)
     elif choose_room == '2':
-        hotel_rooms['107'] = 'Hol Horse'
+        change_room = input('Please enter room you would like to empty: ')
+        hotel_rooms[change_room] = ''
         print(hotel_rooms)
+    elif choose_room == '3':
+        check_hotel_room = input('Please choose room number you would like to check: ')
+        print(hotel_rooms.get(check_hotel_room, 'Room is empty'))
+    elif choose_room == '4':
+        check_hotel_room2 = input('Please enter room you would like to check if occupied: ')
+        print(hotel_rooms.get(check_hotel_room2, 'room is empty'))
+        #if hotel_rooms[check_hotel_room2]
+    else:
+        break
 
+    # hotel_rooms['104'] = ''
+    # print(hotel_rooms)
 
-    hotel_rooms['104'] = ''
-    print(hotel_rooms)
-
-    print(hotel_rooms.get('105', 'Nothing there'))
+    # print(hotel_rooms.get('105', 'Nothing there'))
+print('Thank you for checking hotel rooms!')
