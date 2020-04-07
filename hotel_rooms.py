9de466477a5df25aa6dc492228e98a506f6e67f2
Jack Skiles <jackskiles420@gmail.com>
2020-04-07 11:48:58 -0400

    Updated hotel_rooms.py

--- hotel_rooms.py
+++ hotel_rooms.py
@@ -1,6 +1,18 @@
+# - Make sure you can 
+#     - put someone in an unoccpied room
+#     - make a room available by setting the occupant name to ''
+#     - check if a room number is valid
+#     - check if a room is occupied or not
+
 
 hotel_rooms = {'101': 'Jotaro', '102': 'Joseph', '103': 'kakyoin', '104': 'Avdol', '105': 'Polnereffe', '106': 'Iggy'}
-
 print(hotel_rooms)
+
 hotel_rooms['107'] = 'Hol Horse'
 print(hotel_rooms)
+
+
+hotel_rooms['104'] = ''
+print(hotel_rooms)
+
+print(hotel_rooms.get('105', 'Nothing there'))
